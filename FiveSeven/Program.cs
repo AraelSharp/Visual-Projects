e930ac31290564b04659c1ac6b236dc7efdb172b
Arael <Arael420@protonmail.com>
2019-02-04 16:51:01 +0200

    2more

---
+++ FiveSeven/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FiveSeven
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string test;
+            int inputnumb;
+
+            Console.WriteLine("Enter your number:");
+            test = Console.ReadLine();
+            inputnumb = Convert.ToInt32(test);
+
+            if (inputnumb % 7 == 0)
+            {
+                Console.WriteLine("True Number");
+            }
+            else if (inputnumb % 5 == 0)
+            {
+                Console.WriteLine("True Number");
+            }
+            else
+            {
+                Console.WriteLine("False Number");
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ OddsAndEvens/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OddsAndEvens
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string test;
+            int inputnumb;
+
+            Console.WriteLine("Enter your number:");
+            test = Console.ReadLine();
+            inputnumb = Convert.ToInt32(test);
+
+           if (inputnumb % 2 == 0)
+            {
+                Console.WriteLine("The number you entered is even ;^)");
+            }
+           else
+            {
+                Console.WriteLine("The number you entered is odd ;^(");
+            }
+
+            Console.ReadLine();
+
+        }
+    }
+}
