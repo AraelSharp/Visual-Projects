4d375768d81b7efb0dda1e3f1424522a4ff23daa
Arael <Arael420@protonmail.com>
2019-02-05 20:18:42 +0200

    anotherone

---
+++ 3Sum/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3Sum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string testString;
+            int sumNumbOne;
+            int sumNumbTwo;
+            int sumNumbThree;
+
+            Console.WriteLine("Enter 1st number:");
+            testString = Console.ReadLine();
+            sumNumbOne = int.Parse(testString);
+
+            Console.WriteLine("Enter 2nd number:");
+            testString = Console.ReadLine();
+            sumNumbTwo = int.Parse(testString);
+
+            Console.WriteLine("Enter 3rd number:");
+            testString = Console.ReadLine();
+            sumNumbThree = int.Parse(testString);
+
+            Console.Write("The sum of all numbers is: ");
+            Console.Write(sumNumbOne + sumNumbTwo + sumNumbThree);
+
+            Console.ReadLine();
+        }
+    }
+}
