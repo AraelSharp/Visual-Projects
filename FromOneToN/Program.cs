6973831d822ace154909b284a741682a64a8c6aa
Arael <Arael420@protonmail.com>
2019-02-06 21:51:35 +0200

    Legit

---
+++ FromOneToN/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FromOneToN
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string SuperString;
+            int NumberN;
+
+            Console.WriteLine("Number N: ");
+            SuperString = Console.ReadLine();
+            NumberN = int.Parse(SuperString);
+
+            do
+            {
+                NumberN --;
+            }
+            while (NumberN > 1);
+
+
+            Console.WriteLine();
+            Console.ReadLine();
+
+        }
+    }
+}
