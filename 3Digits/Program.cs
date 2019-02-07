df733be9ae3b71a28fcc6a7e535dee437f7e1323
Arael <Arael420@protonmail.com>
2019-02-07 23:51:20 +0200

    DontTellAnyone

---
+++ 3Digits/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3Digits
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          //TOO HARD USE GOOGLE.           
+        }
+    }
+}
